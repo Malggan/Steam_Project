7d81703af2d8344433469ce34fcc82530ff2a937
Federico Mamani <114963150+Malggan@users.noreply.github.com>
2024-05-27 23:56:10 -0300

    Update

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-import dask.dataframe as dd
 
+app = FastAPI()
 
-df_developer = pd.read_parquet('Dataset/developer.parquet')
-df_user_data_final = pd.read_parquet('Dataset/user_data.parquet')
-df_best_developer = pd.read_parquet('Dataset/developer_of_year.parquet')
-df_developer_reviews = pd.read_parquet('Dataset/developer_review.parquet')
-df_modelo_recomendacion = pd.read_parquet('Dataset/recomendar_juego.parquet')
+def load_developer_data():
+    return pd.read_parquet('Dataset/developer.parquet')
+
+def load_user_data_final():
+    return pd.read_parquet('Dataset/user_data.parquet')
+
+def load_user_genre():
+    return pd.read_parquet('Dataset/user_for_genre.parquet')
+
+def load_best_developer():
+    return pd.read_parquet('Dataset/developer_of_year.parquet')
+
+def load_developer_reviews():
+    return pd.read_parquet('Dataset/developer_review.parquet')
+
+def load_modelo_recomendacion():
+    return pd.read_parquet('Dataset/recomendar_juego.parquet')
 
 
 def developer(desarrolladora):
+    df_developer = load_developer_data()
     developer_filtrado = df_developer[df_developer['developer'] == desarrolladora]
     cantidad_anio = developer_filtrado.groupby('release_year')['item_id'].count()
     gratis_anio = (developer_filtrado[developer_filtrado['price'] == 0.0].groupby('release_year')['item_id'].count() / cantidad_anio * 100).fillna(0).astype(int)
 
-    # Convertir los valores de las Series a listas
     cantidad_anio_list = cantidad_anio.tolist()
     gratis_anio_list = gratis_anio.tolist()
-
-    # Convertir los índices a una lista
     anios_list = cantidad_anio.index.tolist()
 
-    # Crear un diccionario con los resultados
     resultados = {
         'Año': anios_list,
         'Cantidad de juegos': cantidad_anio_list,
@@ -36,9 +45,9 @@ def developer(desarrolladora):
 
 
 def user_data(usuario):
+    df_user_data_final = load_user_data_final()
     user_filtrado = df_user_data_final[df_user_data_final['user_id'] == usuario]
     if not user_filtrado.empty:
-        # Convertir los valores de NumPy a tipos nativos de Python usando int() y float()
         cantidad_dinero = int(user_filtrado['total_spent'].iloc[0])
         items_totales = int(user_filtrado['item_count'].iloc[0])
         total_recomendaciones = float(user_filtrado['recommend'].iloc[0])
@@ -54,57 +63,52 @@ def user_data(usuario):
 
 
 def user_for_genre(genero):
-    # Leer el DataFrame usando Dask
-    ddf = dd.read_parquet('Dataset/user_for_genre.parquet')
-    
-    # Filtrar por género
-    ddf_genero = ddf[ddf['genres'] == genero]
-    
-    # Usuario con más horas jugadas por género
-    usuario_mas_horas = ddf_genero.groupby('user_id')['playtime_forever'].sum().idxmax().compute()
-    
-    # Filtrar por el usuario con más horas jugadas
-    df_usuario_mas_horas = ddf_genero[ddf_genero['user_id'] == usuario_mas_horas].compute()
-    
-    # Calcular las horas jugadas por año de lanzamiento
+    df_user_genre = load_user_genre()
+    df_genero = df_user_genre[df_user_genre['genres'] == genero]
+
+    usuario_mas_horas = df_genero.groupby('user_id')['playtime_forever'].sum().idxmax()
+    usuario_mas_horas_df = df_genero[df_genero['user_id'] == usuario_mas_horas].iloc[0]
+
+    df_usuario_mas_horas = df_genero[df_genero['user_id'] == usuario_mas_horas]
     horas_por_anio = df_usuario_mas_horas.groupby('release_year')['playtime_forever'].sum().to_dict()
-    
-    if usuario_mas_horas is None:
-        return {"Error": "No se encontraron usuarios para el género proporcionado."}
-    
-    return {"Usuario con más horas jugadas por género": usuario_mas_horas, 
-            "Género": genero, 
-            "Horas jugadas por año de lanzamiento": horas_por_anio}
+
+    return {
+        "Usuario con más horas jugadas por género": usuario_mas_horas_df['user_id'], 
+        "Género": usuario_mas_horas_df['genres'], 
+        "Horas jugadas por año de lanzamiento": horas_por_anio
+    }
 
 
 def best_developer_year(year):
-    df = pd.DataFrame(df_best_developer)
-    
-    # Filtrar por año y recomendaciones True
-    df_year = df[(df['release_year'] == year) & (df['recommend'] == True)]
+    df_best_developer = load_best_developer()
+    df_year = df_best_developer[(df_best_developer['release_year'] == year) & (df_best_developer['recommend'] == True)]
 
-    # Calcular la cantidad de recomendaciones por desarrollador
     developer_recommendations = df_year.groupby('developer')['recommend'].count().reset_index()
-
-    # Ordenar en orden descendente y obtener el top 3
     top_developers = developer_recommendations.nlargest(3, 'recommend')
 
-    # Crear la lista de resultados con los puestos y los desarrolladores
-    resultado = [
-        {"Puesto 1": top_developers.iloc[0]['developer'], "Recomendaciones": int(top_developers.iloc[0]['recommend'])},
-        {"Puesto 2": top_developers.iloc[1]['developer'], "Recomendaciones": int(top_developers.iloc[1]['recommend'])},
-        {"Puesto 3": top_developers.iloc[2]['developer'], "Recomendaciones": int(top_developers.iloc[2]['recommend'])},
-    ]
+    resultado = []
+    for i in range(min(3, len(top_developers))):
+        resultado.append({
+            f"Puesto {i + 1}": top_developers.iloc[i]['developer'],
+            "Recomendaciones": int(top_developers.iloc[i]['recommend'])
+        })
+
+    while len(resultado) < 3:
+        resultado.append({
+            f"Puesto {len(resultado) + 1}": 'N/A',
+            "Recomendaciones": 0
+        })
 
     return resultado
 
 
 def developer_reviews_analysis(developer):
+    df_developer_reviews = load_developer_reviews()
     df_filtrado = df_developer_reviews[df_developer_reviews['developer'] == developer]
-    
+
     cantidad_positivos = df_filtrado[df_filtrado['sentiment_analysis'] == 2].shape[0]
     cantidad_negativos = df_filtrado[df_filtrado['sentiment_analysis'] == 0].shape[0]
-    
+
     resultado = {
         "Desarrolladora": developer,
         "Análisis de sentimiento": {
@@ -112,52 +116,40 @@ def developer_reviews_analysis(developer):
             "Negativos": cantidad_negativos
         }
     }
-    
+
     return resultado
 
 
 def recomendacion_juego(id_producto):
-    # Filtrar los juegos que coinciden con el nombre dado
+    df_modelo_recomendacion = load_modelo_recomendacion()
     juego_filtrado = df_modelo_recomendacion[df_modelo_recomendacion['item_id'] == id_producto]
 
-    # Obtener los géneros del juego dado
-    genero_juego = set(juego_filtrado['genres'].str.split(',').explode())
+    if juego_filtrado.empty:
+        return {'Error': 'Producto no encontrado'}
 
-    # Filtrar los juegos que tienen al menos 1 género en común con el juego dado
+    genero_juego = set(juego_filtrado['genres'].str.split(',').explode())
     juegos_recomendados = df_modelo_recomendacion[df_modelo_recomendacion['genres'].apply(lambda x: len(set(x.split(',')).intersection(genero_juego)) >= 1)]
 
-    # Calcular la similitud del coseno entre los vectores de géneros de los juegos
-    juegos_recomendados['genres_vector'] = juegos_recomendados['genres'].apply(lambda x: np.array([1 if genre in x else 0 for genre in genero_juego]))
-    juego_filtrado['genres_vector'] = juego_filtrado['genres'].apply(lambda x: np.array([1 if genre in x else 0 for genre in genero_juego]))
-    juegos_recomendados['similarity'] = juegos_recomendados.apply(lambda row: cosine_similarity([row['genres_vector']], [juego_filtrado['genres_vector'].iloc[0]])[0][0], axis=1)
+    juego_filtrado_vector = np.array([1 if genre in juego_filtrado['genres'].iloc[0] else 0 for genre in genero_juego], dtype=np.float64)
+    juegos_recomendados['similarity'] = juegos_recomendados['genres'].apply(lambda x: cosine_similarity([juego_filtrado_vector], [np.array([1 if genre in x else 0 for genre in genero_juego], dtype=np.float64)])[0][0])
 
-    # Ordenar los juegos por similitud y recomendación en orden descendente
     juegos_recomendados = juegos_recomendados.sort_values(['similarity', 'recommend_y'], ascending=[False, False])
-
-    # Seleccionar los 5 juegos con mayor similitud y recomendación
     top_juegos_recomendados = juegos_recomendados.head(5)
 
-    # Obtener la lista de nombres de los juegos recomendados junto con el desarrollador
-    juegos_recomendados_dict = {}
-    juegos_recomendados_dict['Debido a que te gustó ' + juego_filtrado['title'].iloc[0] + ', también podría interesarte...'] = top_juegos_recomendados[['title']].to_dict(orient='records')
+    juegos_recomendados_dict = {'Debido a que te gustó ' + juego_filtrado['title'].iloc[0] + ', también podría interesarte...': top_juegos_recomendados[['title']].to_dict(orient='records')}
 
     return juegos_recomendados_dict
 
 
-# Se instancia la aplicación
-app = FastAPI()
-
-# Funciones
-@app.get(path="/", 
-         response_class=HTMLResponse,
-         tags=["Home"])
+@app.get(path="/", response_class=HTMLResponse, tags=["Home"])
 def home():
-    return
+    return "<h1>Bienvenido a la API de Steam</h1>"
 
 
-@app.get(path = '/develop')
+@app.get(path='/develop')
 def develop(desarrollador: str):
-    return developer(desarrollador)
+    return developer(desarroll
+
 
 
 @app.get('/userdata')
